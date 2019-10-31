bd7c10de7b42c32cf74967c7e45d931980b550e6
Nicola Lagnena <nlagnena@yahoo.it>
2019-10-31 18:56:03 +0100

    add junit test dependencies and basic structure

---
+++ spiceit-injector/src/test/java/gg/neko/spiceit/injector/SpiceItInjectorTest.java
@@ -0,0 +1,15 @@
+package gg.neko.spiceit.injector;
+
+import org.junit.jupiter.api.Test;
+
+class SpiceItInjectorTest {
+
+    @Test
+    void revise() {
+    }
+
+    @Test
+    void testRevise() {
+    }
+
+}
