780f606f5b439f0b6532a9254ca39deb24ba2128
Nicola Lagnena <nlagnena@yahoo.it>
2019-11-05 22:44:57 +0100

    add javadoc for TimeIt

--- spiceit-api/src/main/java/gg/neko/spiceit/annotation/TimeIt.java
+++ spiceit-api/src/main/java/gg/neko/spiceit/annotation/TimeIt.java
@@ -8,14 +8,36 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Logs the execution time of a method.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface TimeIt {
 
+    /**
+     * If a method has multiple SpiceIt annotations, they are processed
+     * sequentially in this order (ascending).
+     *
+     * @return the processing order of this annotation
+     */
     int order() default 9000;
 
+    /**
+     * The pattern to use when logging this method execution time.
+     * See {@link Patterns} for examples.
+     *
+     * @return the log pattern for this method execution time
+     * @see Patterns
+     */
     String logPattern() default Patterns.TIMEIT_DEFAULT;
 
+    /**
+     * The {@link LogLevel} at which to log this method execution time.
+     *
+     * @return the log level for this method execution time
+     * @see LogLevel
+     */
     LogLevel logLevel() default LogLevel.INFO;
 
 }
