b9b5f69b7815df534eb7862b43e0f82924e965b1
Nicola Lagnena <nlagnena@yahoo.it>
2019-11-08 19:25:05 +0100

    add javadoc to Patterns

--- spiceit-api/src/main/java/gg/neko/spiceit/util/Patterns.java
+++ spiceit-api/src/main/java/gg/neko/spiceit/util/Patterns.java
@@ -1,5 +1,71 @@
 package gg.neko.spiceit.util;
 
+import gg.neko.spiceit.annotation.LogIt;
+
+/**
+ * A collection of common log patterns.
+ * <p>
+ * Available placeholders are:
+ * <table>
+ *   <tr>
+ *     <td><center><i>placeholder</i></center></td>
+ *     <td><center><i>substitution</i></center></td>
+ *     <td><center><i>example</i></center></td>
+ *   </tr>
+ *   <tr>
+ *     <td>{@code ${method.class.name}}</td>
+ *     <td>the method declaring class name</td>
+ *     <td>{@code java.lang.String}</td>
+ *   </tr>
+ *   <tr>
+ *     <td>{@code ${method.class.simpleName}}</td>
+ *     <td>the method declaring class simple name</td>
+ *     <td>{@code String}</td>
+ *   </tr>
+ *   <tr>
+ *     <td>{@code ${method.name}}</td>
+ *     <td>the method name</td>
+ *     <td>{@code replaceAll}</td>
+ *   </tr>
+ *   <tr>
+ *     <td>{@code ${method.signature}}</td>
+ *     <td>the method signature</td>
+ *     <td>{@code replaceAll(java.lang.String, java.lang.String)}</td>
+ *   </tr>
+ *   <tr>
+ *     <td>{@code ${method.longName}}</td>
+ *     <td>the method long name</td>
+ *     <td>{@code java.lang.String.replaceAll(java.lang.String,java.lang.String)}</td>
+ *   </tr>
+ *   <tr>
+ *     <td>{@code ${method.return}}</td>
+ *     <td>the method return value</td>
+ *     <td>{@code the pencil is on the table}</td>
+ *   </tr>
+ *   <tr>
+ *     <td>{@code ${method.exception.message}}</td>
+ *     <td>the method exception message</td>
+ *     <td>{@code String index out of range: 420}</td>
+ *   </tr>
+ *   <tr>
+ *     <td>{@code ${method.args}}</td>
+ *     <td>the method arguments</td>
+ *     <td>{@code [nyan, cat]}</td>
+ *   </tr>
+ *   <tr>
+ *     <td>{@code ${method.args[$index]}}, e.g.: {@code ${method.args[1]}}</td>
+ *     <td>the {@code $index}-th method argument, starting at 1</td>
+ *     <td>{@code nyan}</td>
+ *   </tr>
+ *   <tr>
+ *     <td>{@code ${method.time}}</td>
+ *     <td>the method execution time in milliseconds</td>
+ *     <td>{@code 69}</td>
+ *   </tr>
+ * </table>
+ * <p>
+ * Note that not all placeholders are available at all times, e.g.: {@code ${method.exception.message}} is only available in {@link LogIt#errorPattern()}.
+ */
 public class Patterns {
 
     private Patterns() {
